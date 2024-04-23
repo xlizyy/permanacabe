62573ab2aa09648f69d87e3505162710c2941e9c
Ali Mustofa <alimustofx@gmail.com>
2024-04-23 09:03:28 +0700

    Temporary Finished

---
+++ main.js
@@ -0,0 +1,45 @@
+// NAVBAR ON SCROLL //
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 50)
+})
+
+
+// SHOW & HIDE FAQ ANSWER //
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // -- CHANGE ICON -- //
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-question-circle') {
+            icon.className = "uil uil-minus-circle"
+        }else {
+            icon.className = "uil uil-question-circle";
+        }
+    })
+})
+
+// SHOW & HIDE NAV MENU //
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// CLOSE NAV MENU //
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
